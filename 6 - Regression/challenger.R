e81c61d8cc2c17fc6fb05a6a224a35f181c48afc
Chavez <nailsbrokeandfell@gmail.com>
2014-07-29 21:14:58 -0700

    Correlation

--- 6 - Regression/challenger.R
+++ 6 - Regression/challenger.R
@@ -2,3 +2,12 @@ launch <- read.csv("challenger.csv")
 # b = cov(x, y) / var(x)
 b <- cov(launch$temperature, launch$distress_ct) / var(launch$temperature)
 a <- mean(launch$distress_ct) - b * mean(launch$temperature)
+
+
+##
+## Correlations
+##
+r <- cov(launch$temperature, launch$distress_ct) / (sd(launch$temperature) * sd(launch$distress_ct))
+
+# Build in correlation
+r <- cor(launch$temperature, launch$distress_ct)
