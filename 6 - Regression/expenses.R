5e10d7ef59d8d45ea8796d52489b602da227bb4a
Chavez <nailsbrokeandfell@gmail.com>
2014-07-30 21:23:43 -0700

    Insurance performance

--- 6 - Regression/expenses.R
+++ 6 - Regression/expenses.R
@@ -26,3 +26,22 @@ pairs.panels(insurance[c("age", "bmi", "children", "charges")])
 
 # lm() from stats package built in
 ins_model <- lm(charges ~ age + children + bmi + sex + smoker + region, data = insurance)
+
+##
+## Step 4 - Evaluate Performance
+##
+
+summary(ins_model)
+
+##
+## Step 5 - Improve Performance
+##
+
+# Add non-linear age to model
+insurance$age2 <- insurance$age ^ 2
+
+# BMI indicator 1 or 0 transformation
+insurance$bmi30 <- ifelse(insurance$bmi >= 30, 1, 0)
+
+ins_model2 <- lm(charges ~ age + age2 + children + bmi + sex + bmi30*smoker + region, data = insurance)
+summary(ins_model2)
