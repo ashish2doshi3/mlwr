bcfcf9d63f695e5effbcd38ca2dbe6fe3158c603
Chavez <nailsbrokeandfell@gmail.com>
2014-07-30 21:02:27 -0700

    Insurance prepare data

--- 6 - Regression/expenses.R
+++ 6 - Regression/expenses.R
@@ -2,3 +2,20 @@
 ## Step 1 - Gather Data
 ##
 insurance <- read.csv("insurance.csv", stringsAsFactors = TRUE)
+
+##
+## Step 2 - Prepare Data
+##
+summary(insurance)
+
+hist(insurance$charge)
+
+# Check correlations among features
+cor(insurance[c("age", "bmi", "children", "charges")])
+
+# Scatter Plot Matrix
+pairs(insurance[c("age", "bmi", "children", "charges")])
+
+# Use psych package for further info
+library(psych)
+pairs.panels(insurance[c("age", "bmi", "children", "charges")])
