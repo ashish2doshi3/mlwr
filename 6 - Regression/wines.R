5c97efb242c9a3100a411991efaaac0e4cf047df
Chavez <nailsbrokeandfell@gmail.com>
2014-07-31 20:37:29 -0700

    Wine regression train

--- 6 - Regression/wines.R
+++ 6 - Regression/wines.R
@@ -3,6 +3,28 @@
 ##
 wine <- read.csv("whitewines.csv")
 
+##
+## Step 2 - Prepare Data
+##
 str(wine)
 
 hist(wine$quality)
+
+wine_train <- wine[1:3750, ]
+wine_test <- wine[3751:4898, ]
+
+##
+## Step 3 - Train Data
+##
+
+# Use rpart form install.packages("rpart")
+# For fitting classification trees or regression trees
+library(rpart)
+
+m.rpart <- rpart(quality ~., data = wine_train)
+m.rpart
+
+# Plot using install.packages("rpart.plot")
+library(rpart.plot)
+
+rpart.plot(m.rpart, digits = 3)
