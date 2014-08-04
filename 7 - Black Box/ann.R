88ad13f0a11e81c9c7a03dba17ba1b52461c9c2e
Chavez <nailsbrokeandfell@gmail.com>
2014-08-03 21:08:12 -0700

    ANN prepare data

--- 7 - Black Box/ann.R
+++ 7 - Black Box/ann.R
@@ -3,3 +3,18 @@
 ##
 concrete <- read.csv("concrete.csv")
 str(concrete)
+
+##
+## Step 2 - Prepare Data
+##
+
+# Normalize function
+normalize <- function(x) {
+        return ((x - min(x)) / (max(x) - min(x)))
+}
+
+concrete_norm <- as.data.frame(lapply(concrete, normalize))
+summary(concrete_norm$strength)
+
+concrete_train <- concrete_norm[1:773, ]
+concrete_test <- concrete_norm[774:1030, ]
