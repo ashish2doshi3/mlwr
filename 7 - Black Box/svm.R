387fafbb6ab7fa0d4f751b4af931c4debdc9cf75
Chavez <nailsbrokeandfell@gmail.com>
2014-08-04 22:14:45 -0700

    SVM train

--- 7 - Black Box/svm.R
+++ 7 - Black Box/svm.R
@@ -11,3 +11,17 @@ letters <- read.csv("letterdata.csv")
 # split into train and test data
 letters_train <- letters[1:16000, ]
 letters_test  <- letters[16001:20000, ]
+
+##
+## Step 3 - Train
+##
+
+# SVM packages
+# 1. e1071 - Award winning from Vienna University (C++)
+# 2. klaR - SVMlight algrorithm
+# 3. kernlab - Native R implementation good for starting out
+
+# install.packages("kernlab")
+library(kernlab)
+
+letter_classifier <- ksvm(letter ~., data = letters_train, kernel = "vanilladot")
