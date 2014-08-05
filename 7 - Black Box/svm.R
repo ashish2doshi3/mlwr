f382eb6448f6d520760f622f2f15ae0a421596ea
Chavez <nailsbrokeandfell@gmail.com>
2014-08-04 22:21:12 -0700

    Performance

--- 7 - Black Box/svm.R
+++ 7 - Black Box/svm.R
@@ -25,3 +25,29 @@ letters_test  <- letters[16001:20000, ]
 library(kernlab)
 
 letter_classifier <- ksvm(letter ~., data = letters_train, kernel = "vanilladot")
+
+##
+## Step 4 - Evaluate Performance
+##
+
+letter_predictions <- predict(letter_classifier, letters_test)
+
+# Show predictions of letters
+head(letter_predictions)
+
+agreement <- letter_predictions == letters_test$letter
+
+table(agreement)
+
+prop.table(table(agreement))
+
+##
+## Step 5 - Improve Performance
+##
+
+letter_classifier_rbf <- ksvm(letter ~., data = letters_train, kernel = "rbfdot")
+letter_predictions_rbf <- predict(letter_classifier_rbf, letters_test)
+
+agreement_rbf <- letter_predictions_rbf == letters_test$letter
+table(agreement_rbf)
+prop.table(table(agreement_rbf))
