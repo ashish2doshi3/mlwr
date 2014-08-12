3e8f146a68331ea8697bfd4dcaa68a4cf2f32cc7
Chavez <nailsbrokeandfell@gmail.com>
2014-08-11 22:00:25 -0700

    K means get and prepare data

---
+++ 9 - K Means/kmeans.R
@@ -0,0 +1,45 @@
+##
+## Step 1 - Collecting Data
+##
+teens <- read.csv("snsdata.csv")
+
+str(teens)
+
+##
+## Step 2 - Prepare Data
+##
+
+# Look at gender split
+table(teens$gender)
+
+# Show NAs
+table(teens$gender, useNA = "ifany")
+
+# Look at age distribution
+# Shows lots of NAs and a min of age 3 and max of age 106
+summary(teens$age)
+
+# Clean up age to exclude 20 and older and under 13
+teens$age <- ifelse(teens$age >= 13 & teens$age < 20, teens$age, NA)
+summary(teens$age)
+
+##
+## Dummy coding of missing ages
+##
+
+# Set binary value of female or not unknown gender
+teens$female <- ifelse(teens$gender == "F" & !is.na(teens$gender), 1, 0)
+
+# Set binary value of no gender or gender known
+teens$no_gender <- ifelse(is.na(teens$gender), 1, 0)
+
+# Get mean age with NAs excluded
+mean(teens$age, na.rm = TRUE)
+
+# Even better get mean age by subgroup of gradyear
+aggregate(data = teens, age ~ gradyear, mean, na.rm = TRUE)
+
+# Use ave() to apply across vectors
+ave_age <- ave(teens$age, teens$gradyear, FUN = function(x) mean(x, na.rm = TRUE))
+
+teens$age <- ifelse(is.na(teens$age), ave_age, teens$age)
