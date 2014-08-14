f0797d67871953f3eb3f3ef402bb4acc2711b6af
Chavez <nailsbrokeandfell@gmail.com>
2014-08-13 22:07:48 -0700

    Model performance

--- 10 - Model Performance/future.R
+++ 10 - Model Performance/future.R
@@ -18,4 +18,26 @@ credit_train <- credit[in_train,]
 credit_test <- credit[-in_train,]
 
 ## Cross Validation
+
+# 10 folds is standard number of splits
 folds <- createFolds(credit$default, k = 10)
+
+library(caret)
+library(C50)
+library(irr)
+
+set.seed(123)
+folds <- createFolds(credit$default, k = 10)
+
+cv_results <- lapply(folds, function(x){
+        credit_train <- credit[x,]
+        credit_test <- credit[-x,]
+        credit_model <- C5.0(default ~., data = credit_train)
+        credit_pred <- predict(credit_model, credit_test)
+        credit_actual <- credit_test$default
+        kappa <- kappa2(data.frame(credit_actual, credit_pred))$value
+        return(kappa)
+})
+
+str(cv_results)
+mean(unlist(cv_results))
