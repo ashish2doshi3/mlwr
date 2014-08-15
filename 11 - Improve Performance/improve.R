96c28106319fb110348c5f4d4d5d47351ff0ea21
Chavez <nailsbrokeandfell@gmail.com>
2014-08-14 20:12:38 -0700

    Improve performance with custom tuning

--- 11 - Improve Performance/improve.R
+++ 11 - Improve Performance/improve.R
@@ -22,3 +22,20 @@ table(p, credit$default)
 head(p)
 
 head(predict(m, credit, type = "prob"))
+
+##
+## Customizing Tuning Process
+##
+
+# Use trainControl() to get a set of config options for train()
+# Here is 10 fold cross validation and oneSE for
+# selecting optimal model
+ctrl <- trainControl(method = "cv", number = 10, selectionFunction = "oneSE")
+
+# Next create a grid of parameters to optimize
+# Holding method to tree and winnow to false
+grid <- expand.grid(.model = "tree", .trials = c(1, 5, 10, 15, 20, 25, 30, 35), .winnow = FALSE)
+
+# Now train using customed tuning settings
+set.seed(300)
+m <- train(default ~., data = credit, method = "C5.0", metric = "Kappa", trControl = ctrl, tuneGrid = grid)
